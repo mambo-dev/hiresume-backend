d3419c2dc03bdb28e3d76c15f5307b2c873b9281
mambo-dev <mambo.michael.22@gmail.com>
2022-12-30 23:19:24 +0300

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import { ValidationPipe } from "@nestjs/common";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: "http://localhost:3000", // allow requests from any origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // allow these methods
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
